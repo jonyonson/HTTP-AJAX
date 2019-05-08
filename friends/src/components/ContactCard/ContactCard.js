94c02b1d323cee63e4e189176478ff736e546757
Jonathan Taylor <jonyonson@icloud.com>
2019-05-08 18:38:35 -0500

    icons for editing and deleting added

--- friends/src/components/ContactCard/ContactCard.js
+++ friends/src/components/ContactCard/ContactCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ContactCard.css';
+import { MdDeleteForever, MdEdit } from 'react-icons/md';
 
 const ContactCard = ({ contact }) => {
   return (
@@ -8,6 +9,10 @@ const ContactCard = ({ contact }) => {
       <div className="ContactCard__email">{contact.email}</div>
       <div className="ContactCard__phone">{contact.phone}</div>
       <div className="ContactCard__age">age: {contact.age}</div>
+      <div className="ContactCard__icons">
+        <MdEdit className="edit-icon" size="20" />
+        <MdDeleteForever className="delete-icon" size="22" />
+      </div>
     </div>
   );
 };
